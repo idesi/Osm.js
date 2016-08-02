4f5a45a640c3e607440f99c37a8192e9ecf6fd7f
Watandeep Sekhon <watan.cp@gmail.com>
2016-08-01 23:42:46 -0400

    Check in library core

---
+++ src/index.js
@@ -0,0 +1,53 @@
+const objIdentifier = '[object Object]';
+
+const curry = (fn) => {
+  const arity = fn.length;
+
+  const curried = (...args) => {
+    if (args.length < arity) {
+      return (...remaining) => curried(...args, ...remaining);
+    }
+    return fn(...args);
+  };
+
+  return curried;
+};
+
+const map = curry((fn, obj) => {
+  return Object.keys(obj)
+    .reduce((acc, current) => {
+      acc[current] = fn(obj[current]);
+      return acc;
+    }, {});
+});
+
+const filter = curry((predicate, obj) => {
+  return Object.keys(obj)
+    .filter(key => predicate(obj[key], key))
+    .reduce((res, key) => {
+      res[key] = obj[key];
+      return res;
+    }, {});
+});
+
+const isObject = (obj) => Object.prototype.toString.call(obj) === objIdentifier;
+
+const clone = (obj) => Object.assign(Object.create(obj), obj);
+
+const extend = (...args) => {
+  return args.reduce((acc, curr) => {
+    Object.assign(acc, curr);
+    return acc;
+  }, {});
+};
+
+const omit = (obj, keys) => filter((val, key) => keys.indexOf(key) === -1, obj);
+
+export default {
+  map,
+  filter,
+  isObject,
+  clone,
+  extend,
+  omit
+};
---
+++ test/osm.spec.js
@@ -0,0 +1,110 @@
+import {expect, assert} from 'chai';
+import osm from '../lib/osm.js';
+
+const scores = {
+  Jason: 2,
+  Sam: 3,
+  Jane: 1
+}
+
+const doubleScores = {
+  Jason: 4,
+  Sam: 6,
+  Jane: 2
+}
+
+const filteredScores = {
+  Jason: 2,
+  Sam: 3
+}
+
+const double = (v) => v * 2;
+const predicate = score => score > 1;
+
+describe('osm', () => {
+  describe('map', () => {
+    it('should return a function when obj is not provided', () => {
+      assert.isFunction((osm.map(double)));
+    });
+    it('should create a new object after remaining arguments are provided', () => {
+      const dbl = osm.map(double);
+      expect(dbl(scores)).to.eql(doubleScores);
+      expect(scores).to.not.eql(doubleScores);
+    });
+    it('should create a new object when all arguments are provided', () => {
+      expect(osm.map(double, scores)).to.eql(doubleScores);
+    });
+  });
+
+  describe('filter', () => {
+    it('should return a function when obj is not provided', () => {
+      assert.isFunction((osm.filter(predicate)));
+    });
+
+    it('should return filtered object after remianing args are provided', () => {
+      const filterFn = osm.filter(predicate);
+      expect(filterFn(scores)).to.eql(filteredScores);
+      expect(scores).to.not.eql(filteredScores);
+    })
+
+    it('should return new obj after filtering out data', () => {
+      expect(osm.filter(predicate, scores)).to.eql(filteredScores);
+      expect(scores).to.not.eql(filteredScores);
+    });
+  });
+
+  describe('isObject', () => {
+    it('should return true if parameter is an object', () => {
+      expect(osm.isObject({})).to.be.true;
+    });
+
+    it('should return false if parameter is an array', () => {
+      expect(osm.isObject([])).to.be.false;
+    });
+
+    it('should return false if parameter is a string', () => {
+      expect(osm.isObject('')).to.be.false;
+    });
+
+    it('should return false if parameter is a number', () => {
+      expect(osm.isObject(0)).to.be.false;
+    });
+
+    it('should return false if parameter is undefined', () => {
+      expect(osm.isObject(undefined)).to.be.false;
+    });
+
+    it('should return false if parameter is null', () => {
+      expect(osm.isObject(null)).to.be.false;
+    });
+
+    it('should return false if parameter is NaN', () => {
+      expect(osm.isObject(NaN)).to.be.false;
+    });
+  });
+
+  describe('clone', () => {
+    it('should create a shallow clone of the object', () => {
+      expect(osm.clone(scores)).to.eql(scores);
+    });
+  });
+
+  describe('extend', () => {
+    it('should copy all properties from source into destination.', () => {
+      const numbers = {One: 1, Two: 2, Three: 3};
+      const strings = {One: 'One', Two: 'Two'};
+      const output = {One: 'One', Two: 'Two', Three: 3};
+      const extended = osm.extend(numbers, strings);
+      expect(extended).to.eql(output);
+      expect(numbers).to.not.eql(extended);
+    });
+  });
+
+  describe('omit', () => {
+    it('should return a new obj with certain properties ommitted', () => {
+      const omitScores = osm.omit(scores, ['Jason', 'Sam']);
+      expect(omitScores).to.eql({Jane: 1});
+      expect(scores).to.not.eql(omitScores);
+    });
+  });
+});
